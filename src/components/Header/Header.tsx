dca97bd3521b9e5df22df3b269817d1589c1c543
Dmitriy_Zakirov <metrio@inbox.ru>
2026-07-04 18:56:21 +0500

    feat(header-hero): add adaptive design

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import { Link, NavLink } from "react-router-dom";
 import styles from "./Header.module.scss";
 
-export default function Header() {
+export default function Header() {  
   return (
     <header className={styles.header}>
       <div className={styles.container}>
